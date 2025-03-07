7b4a531f46ff303dc188fcf73660bf763575875f
Khoerunissa99 <khoerunissa12nis@gmail.com>
2025-03-07 23:18:44 +0700

    Update AngkaSialException.java

--- PBO 3/AngkaSialException.java
+++ PBO 3/AngkaSialException.java
@@ -1,3 +1,8 @@
+// Nama : Khoerunissa Billah Wahyu Sarbini
+// NIM  : 24060123140178
+// Lab : C1
+// File : AngkaSialExceptio.java
+
 public class AngkaSialException extends Exception {
     public AngkaSialException(){
         super("jangan memasukan angka 13 karena angka sial !!!");
